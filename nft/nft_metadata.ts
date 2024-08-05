cac3707bdaec1487f61e07bb84fb80aac8041101
mompei <ogunborjerry@gmail.com>
2024-08-05 14:53:30 +0100

    add nft

---
+++ nft/nft_metadata.ts
@@ -0,0 +1,48 @@
+import wallet from "../../wba-wallet.json";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
+import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
+
+// Create a devnet connection
+const umi = createUmi('https://api.devnet.solana.com');
+
+let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
+const signer = createSignerFromKeypair(umi, keypair);
+
+umi.use(irysUploader());
+umi.use(signerIdentity(signer));
+
+(async () => {
+    try {
+        // Follow this JSON structure
+        // https://docs.metaplex.com/programs/token-metadata/changelog/v1.0#json-structure
+
+        const image = "https://arweave.net/BPP_XA1bhO6gN3bGP17GLxTz8C15FcdEQMHpWs-sMak"
+        const metadata = {
+            name: "MICHAEL JACKSON",
+            symbol: "MJ",
+            description: "MJ SEMPRE",
+            image,
+            attributes: [
+                {trait_type: 'Color', value: 'Inconclusive'},
+                {trait_type: 'Size', value: '90'},
+                
+            ],
+            properties: {
+                files: [
+                    {
+                        type: "image/png",
+                        uri: image
+                    },
+                ]
+            },
+            creators: []
+        };
+        
+        const myUri = await umi.uploader.uploadJson(metadata);
+        console.log("Your image URI: ", myUri);
+    }
+    catch(error) {
+        console.log("Oops.. Something went wrong", error);
+    }
+})();
