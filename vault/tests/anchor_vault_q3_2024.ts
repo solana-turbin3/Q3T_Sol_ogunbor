96a511154aa678dedb8ebc9ee6bd61324b7984fb
mompei <ogunborjerry@gmail.com>
2024-08-11 08:50:17 +0100

    add vault files

---
+++ vault/tests/anchor_vault_q3_2024.ts
@@ -0,0 +1,36 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { AnchorVaultQ32024 } from '../target/types/anchor_vault_q3_2024';
+import { PublicKey, Keypair, SystemProgram } from "@solana/web3.js";
+import { BN } from "bn.js";
+
+describe("anchor_vault_q3_2024", () => {
+  // Configure the client to use the local cluster.
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.AnchorVaultQ32024 as Program<AnchorVaultQ32024>;
+  
+  const vaultState = anchor.web3.PublicKey.findProgramAddressSync([Buffer.from("state"),
+  provider.publicKey.toBytes()],
+    program.programId)[0];
+    
+  const vault = anchor.web3.PublicKey.findProgramAddressSync([Buffer.from("vault"),
+  vaultState.toBytes()],
+    program.programId)[0];
+    
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().accountsPartial(
+      {
+        user: provider.wallet.publicKey,
+        vaultState,
+        vault,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      }
+    ).rpc();
+    console.log("Your transaction signature", tx);
+  });
+  
+});
+
